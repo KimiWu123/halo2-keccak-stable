409b2f83871cbca77acd14e7e1dc89dbb2a57fbb
Artem <48178500+ElusAegis@users.noreply.github.com>
2024-07-11 23:27:23 +0100

    (fix: lib) Fixed an issue with the library functions not being exported

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,15 @@
 //! The circuits can be configured to use different numbers of columns, by specifying the number of rows per internal
 //! round of the keccak_f permutation.
 
+pub use circuit::{KeccakCircuit, prove, verify};
+pub use vanilla::KeccakConfigParams;
+
 /// Module for Keccak circuits in vanilla halo2.
 mod vanilla;
 mod util;
 
 mod circuit;
 pub mod io;
-
-use std::collections::HashMap;
-use halo2_proofs::halo2curves::bn256::{Bn256, Fr, G1Affine};
-use halo2_proofs::plonk::ProvingKey;
-use halo2_proofs::poly::kzg::commitment::ParamsKZG;
-pub use circuit::KeccakCircuit;
-pub use vanilla::KeccakConfigParams;
-use crate::circuit::unpack_input;
 
 pub const DEFAULT_ROWS_PER_ROUND: usize = 28;
 pub const DEFAULT_K: u32 = 14;
